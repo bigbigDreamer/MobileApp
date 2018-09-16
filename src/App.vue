158c9782537d41acc72738d5ebc261bc0622d664
bigDreamer <1826001146@qq.com>
2018-09-16 20:41:04 +0800

    :house_with_garden:主入口配置

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <b-container>
+      <router-view></router-view>
+    </b-container>
+</template>
+
+<script>
+  import Login from './components/Login'
+  import {mapState} from 'vuex'
+export default {
+  name: 'App',
+  components:{
+    Login
+  },
+  computed: {
+    ...mapState([
+      'status'
+    ])
+  },
+  watch:{
+    status:function () {
+       if(this.status === 1) {
+         this.$router.push({path:'/login'});
+       }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
